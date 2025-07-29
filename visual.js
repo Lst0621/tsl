6c69a382914e35b7705cb7ca5ed50d5e9615d871
Haojun Lu <haojunlu@foxmail.com>
2025-07-28 18:43:56 -0700

    Update Draw Table

--- visual.js
+++ visual.js
@@ -1,4 +1,5 @@
-export function draw_multiplication_table(table, input, multiply, to_string, input_get_color, product_get_color) {
+// Row, Column, Element
+export function draw_table(table, rows, cols, multiply, rows_to_string, cols_to_string, element_to_string, row_get_color, col_get_color, element_get_color) {
     while (true) {
         if (table.rows.length == 0) {
             break;
@@ -11,25 +12,28 @@ export function draw_multiplication_table(table, input, multiply, to_string, inp
     {
         let row = table.insertRow();
         row.insertCell();
-        for (let i = 0; i < input.length; i++) {
+        for (let i = 0; i < cols.length; i++) {
             let cell = row.insertCell();
             cell.style.borderStyle = "solid";
-            cell.innerHTML = to_string(input[i]);
-            cell.style.background = input_get_color(input[i]);
+            cell.innerHTML = cols_to_string(cols[i]);
+            cell.style.background = col_get_color(cols[i]);
         }
     }
-    for (let i = 0; i < input.length; i++) {
+    for (let i = 0; i < rows.length; i++) {
         let row = table.insertRow();
         let cell = row.insertCell();
         cell.style.borderStyle = "solid";
-        cell.style.background = input_get_color(input[i]);
-        cell.innerHTML = to_string(input[i]);
-        for (let j = 0; j < input.length; j++) {
+        cell.style.background = row_get_color(rows[i]);
+        cell.innerHTML = rows_to_string(rows[i]);
+        for (let j = 0; j < cols.length; j++) {
             let cell_product = row.insertCell();
             cell_product.style.borderStyle = "solid";
-            let product = multiply(input[i], input[j]);
-            cell_product.innerHTML = to_string(product);
-            cell_product.style.background = product_get_color(input[i], input[j], product);
+            let element = multiply(i, j);
+            cell_product.innerHTML = element_to_string(element);
+            cell_product.style.background = element_get_color(i, j);
         }
     }
+}
+export function draw_multiplication_table(table, input, multiply, to_string, input_get_color, product_get_color) {
+    draw_table(table, input, input, (i, j) => multiply(input[i], input[j]), to_string, to_string, to_string, input_get_color, input_get_color, (i, j) => product_get_color(input[i], input[j], multiply(input[i], input[j])));
 }
--- visual.ts
+++ visual.ts
@@ -1,10 +1,15 @@
-export function draw_multiplication_table<T>(
+// Row, Column, Element
+export function draw_table<R, C, E>(
     table: HTMLTableElement,
-    input: T[],
-    multiply: (a: T, b: T) => T,
-    to_string: (a: T) => string,
-    input_get_color: (a: T) => string,
-    product_get_color: (a: T, b: T, c: T) => string
+    rows: R[],
+    cols: C[],
+    multiply: (row: number, col: number) => E,
+    rows_to_string: (a: R) => string,
+    cols_to_string: (a: C) => string,
+    element_to_string: (a: E) => string,
+    row_get_color: (a: R) => string,
+    col_get_color: (a: C) => string,
+    element_get_color: (row: number, col: number) => string
 ) {
     while (true) {
         if (table.rows.length == 0) {
@@ -19,28 +24,50 @@ export function draw_multiplication_table<T>(
     {
         let row = table.insertRow();
         row.insertCell();
-        for (let i = 0; i < input.length; i++) {
+        for (let i = 0; i < cols.length; i++) {
             let cell = row.insertCell()
 
             cell.style.borderStyle = "solid"
-            cell.innerHTML = to_string(input[i])
-            cell.style.background = input_get_color(input[i])
+            cell.innerHTML = cols_to_string(cols[i])
+            cell.style.background = col_get_color(cols[i])
 
         }
     }
 
-    for (let i = 0; i < input.length; i++) {
+    for (let i = 0; i < rows.length; i++) {
         let row = table.insertRow()
         let cell = row.insertCell()
         cell.style.borderStyle = "solid"
-        cell.style.background = input_get_color(input[i])
-        cell.innerHTML = to_string(input[i])
-        for (let j = 0; j < input.length; j++) {
+        cell.style.background = row_get_color(rows[i])
+        cell.innerHTML = rows_to_string(rows[i])
+        for (let j = 0; j < cols.length; j++) {
             let cell_product = row.insertCell()
             cell_product.style.borderStyle = "solid"
-            let product: T = multiply(input[i], input[j])
-            cell_product.innerHTML = to_string(product)
-            cell_product.style.background = product_get_color(input[i], input[j], product)
+            let element: E = multiply(i, j)
+            cell_product.innerHTML = element_to_string(element)
+            cell_product.style.background = element_get_color(i, j)
         }
     }
+}
+
+export function draw_multiplication_table<T>(
+    table: HTMLTableElement,
+    input: T[],
+    multiply: (a: T, b: T) => T,
+    to_string: (a: T) => string,
+    input_get_color: (a: T) => string,
+    product_get_color: (a: T, b: T, c: T) => string
+) {
+    draw_table(
+        table,
+        input,
+        input,
+        (i: number, j: number) => multiply(input[i], input[j]),
+        to_string,
+        to_string,
+        to_string,
+        input_get_color,
+        input_get_color,
+        (i: number, j: number) => product_get_color(input[i], input[j], multiply(input[i], input[j]))
+    )
 }
