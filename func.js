44c1c33dbfe5bde9a3a3b46ae238a20b0463bdbb
Haojun Lu <haojunlu@foxmail.com>
2025-09-05 22:37:06 -0700

    Add Function Compose and Power

--- func.js
+++ func.js
@@ -1,3 +1,32 @@
 export function always(a) {
     return (...args) => a;
 }
+export function function_compose(f, g) {
+    return (a) => f(g(a));
+}
+export function function_power(f, n) {
+    return function_power_v2(f, n);
+}
+export function identity(x) {
+    return x;
+}
+export function function_power_v1(f, n) {
+    // This may cause stack overflow.
+    let power = (x) => x;
+    for (let i = 0; i < n; i++) {
+        power = function_compose(power, f);
+    }
+    return power;
+}
+export function function_power_v2(f, n) {
+    if (n == 0) {
+        return identity;
+    }
+    return (x) => {
+        let result = x;
+        for (let i = 0; i < n; i++) {
+            result = f(result);
+        }
+        return result;
+    };
+}
--- func.ts
+++ func.ts
@@ -1,3 +1,37 @@
 export function always<T>(a: T): (...args: any[]) => T {
     return (...args: any[]) => a;
 }
+
+export function function_compose<A, B, C>(f: (b: B) => C, g: (a: A) => B): (a: A) => C {
+    return (a: A) => f(g(a));
+}
+
+export function function_power<T>(f: (x: T) => T, n: number): (x: T) => T {
+    return function_power_v2(f, n)
+}
+
+export function identity<T>(x: T): T {
+    return x;
+}
+
+export function function_power_v1<T>(f: (x: T) => T, n: number): (x: T) => T {
+    // This may cause stack overflow.
+    let power = (x: T) => x
+    for (let i = 0; i < n; i++) {
+        power = function_compose(power, f)
+    }
+    return power
+}
+
+export function function_power_v2<T>(f: (x: T) => T, n: number): (x: T) => T {
+    if (n == 0) {
+        return identity
+    }
+    return (x: T) => {
+        let result = x
+        for (let i = 0; i < n; i++) {
+            result = f(result)
+        }
+        return result
+    }
+}
