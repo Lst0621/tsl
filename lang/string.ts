f42293e305aa585ac88f00ea09fa1779c50bfbda
Haojun Lu <haojunlu@foxmail.com>
2025-09-14 15:48:07 -0700

    Sub Sequence Func

--- lang/string.ts
+++ lang/string.ts
@@ -1,5 +1,7 @@
 import {cartesian_product} from "../math/set.js";
 import {get_sup} from "../util.js";
+import {generate_monoid} from "../math/monoid.js";
+import {array_eq} from "../math/math.js";
 
 export function get_all_prefixes(str: string): string[] {
     const prefixes: string[] = [];
@@ -51,4 +53,67 @@ export function sub_empty_with_ep(str: string) {
         return "ε"
     }
     return str;
+}
+
+let NO_LENGTH_LIMIT = -1
+
+export function cat_subseq_leq_q(s1: string[], s2: string[], k: number = -1) {
+    return concat_string_lists(s1, s2).filter(s => (k == NO_LENGTH_LIMIT || s.length <= k))
+}
+
+export function cat_subseq_of_blocklist(s1: string[], s2: string[], k: number, blocks: string[]) {
+    return cat_subseq_leq_q(s1, s2, k).filter(s => blocks.some(block => is_sub_seq(block, s)))
+}
+
+export function get_all_subseq_leq_k(str: string, k: number = -1): string[] {
+    if (str.length == 0) {
+        return [""]
+    }
+    if (str.length == 1) {
+        return (k == NO_LENGTH_LIMIT || k >= 1) ? [str, ""] : [""]
+    }
+
+    let half = Math.trunc(str.length / 2)
+    let first_half = get_all_subseq_leq_k(str.substring(0, half), k)
+    let second_half = get_all_subseq_leq_k(str.substring(half, str.length), k)
+    return cat_subseq_leq_q(first_half, second_half, k)
+}
+
+export function get_subseq_of_blocklist(str: string, blocks: string[]): string[] {
+    let k = Math.max(...(blocks.map(s => s.length)))
+    if (str.length <= 1) {
+        return get_all_subseq_leq_k(str).filter(s => blocks.some(block => is_sub_seq(block, s)))
+    }
+
+    let half = Math.trunc(str.length / 2)
+    let first_half = get_subseq_of_blocklist(str.substring(0, half), blocks)
+    let second_half = get_subseq_of_blocklist(str.substring(half, str.length), blocks)
+    return cat_subseq_of_blocklist(first_half, second_half, k, blocks)
+}
+
+export function is_sub_seq(str: string, pattern: string) {
+    if (str.length < pattern.length) {
+        return false
+    }
+    if (pattern.length == 0) {
+        return true;
+    }
+
+    // TODO for loop instead of recursive
+    if (str.charAt(0) == pattern.charAt(0)) {
+        return is_sub_seq(str.substring(1, str.length), pattern.substring(1, pattern.length));
+    } else {
+        return is_sub_seq(str.substring(1, str.length), pattern);
+    }
+}
+
+export function get_all_subseq_for_blocks(blocks: string[]) {
+    let alphabet: string[] = get_alphabet_from_strings(blocks)
+    let k = Math.max(...(blocks.map(s => s.length)))
+    let generators: string[][] = alphabet.map(x => [x, ""])
+    generators.push([""])
+    let concat = (s1: string[], s2: string[]) =>
+        cat_subseq_of_blocklist(s1, s2, k, blocks)
+    let all_subs = generate_monoid(generators, concat, array_eq)
+    return all_subs
 }
