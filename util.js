3e1b3751532030b00626f7e4ae9a158450552c9f
Haojun Lu <haojunlu@foxmail.com>
2025-10-11 16:56:44 -0700

    Add Random Fill Function

--- util.js
+++ util.js
@@ -16,3 +16,21 @@ export function range(start, end) {
 export function create_2d_array(m, n, a) {
     return Array.from({ length: m }, () => Array(n).fill(a));
 }
+export function random_fill(array, threshold) {
+    let l1 = array.length;
+    let l2 = array[0].length;
+    let new_array = [];
+    for (let i = 0; i < l1; i++) {
+        let row = [];
+        for (let j = 0; j < l2; j++) {
+            if (Math.random() < threshold) {
+                row.push(1);
+            }
+            else {
+                row.push(0);
+            }
+        }
+        new_array.push(row);
+    }
+    return new_array;
+}
--- util.ts
+++ util.ts
@@ -19,3 +19,21 @@ export function range(start: number, end: number) {
 export function create_2d_array<T>(m: number, n: number, a: T): T[][] {
     return Array.from({ length: m }, () => Array(n).fill(a));
 }
+
+export function random_fill(array: number[][], threshold: number): number[][] {
+    let l1 = array.length;
+    let l2 = array[0].length;
+    let new_array: number[][] = [];
+    for (let i: number = 0; i < l1; i++) {
+        let row: number[] = [];
+        for (let j: number = 0; j < l2; j++) {
+            if (Math.random() < threshold) {
+                row.push(1);
+            } else {
+                row.push(0);
+            }
+        }
+        new_array.push(row);
+    }
+    return new_array;
+}
