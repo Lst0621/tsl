14839cb1f079edaa6b371ecc02f9bcb892fe69c4
Haojun Lu <haojunlu@foxmail.com>
2026-03-07 16:07:10 -0800

    ClangD

--- wasm/build.sh
+++ wasm/build.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")" || exit 1
 
 # Configure with explicit Emscripten toolchain
 cmake -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DCMAKE_C_COMPILER=/usr/bin/emcc \
   -DCMAKE_CXX_COMPILER=/usr/bin/em++ \
   -S . \
